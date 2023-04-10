8ca9276aef6654e248a5b3b0a7e7206248fc97ff
astro543 <kinyuab54@gmail.com>
2023-04-09 23:09:24 -0700

    my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that prints its own name
+ * @argc: number of arguments passed to the program
+ * @argv: array of arguments
+ * Return: Alway 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+}
